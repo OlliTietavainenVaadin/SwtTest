ff2ab0320f034571d4cb57a16f478d6f3885461b
Olli Tietäväinen <ollit@vaadin.com>
2015-12-22 14:39:34 +0200

    added swt browser for side-by-side comparisons

--- src/com/vaadin/demo/SwtDemo.java
+++ src/com/vaadin/demo/SwtDemo.java
@@ -6,11 +6,11 @@ import javax.swing.JApplet;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
@@ -19,11 +19,15 @@ import org.eclipse.swt.widgets.Shell;
 
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+import com.teamdev.jxbrowser.chromium.events.StartLoadingEvent;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
 public class SwtDemo {
     private Shell shell;
     private Browser browser = new Browser();
+    private org.eclipse.swt.browser.Browser swtBrowser;
     
     public static void main(String[] args) {
         Display display = new Display();
@@ -41,19 +45,51 @@ public class SwtDemo {
         shell.setLayout(new FillLayout());
         createMenuBar();
         shell.setText("JxBrowser demo");
-
+        swtBrowser  = new org.eclipse.swt.browser.Browser(shell, SWT.NONE);
+        swtBrowser.addProgressListener(new ProgressListener() {
+            int i = 0;
+            
+            @Override
+            public void completed(ProgressEvent event) {
+                System.out.println("SWT browser completed: " + event.toString());
+                System.out.println("I is now: " + i);
+                
+            }
+            
+            @Override
+            public void changed(ProgressEvent event) {
+                System.out.println("SWT browser changed: " + event.toString());
+                i++;
+            }
+        });
+        
+        browser.addLoadListener(new LoadAdapter() {
+            @Override
+            public void onStartLoadingFrame(StartLoadingEvent event) {
+                if (event.isMainFrame()) {
+                    System.out.println("Main frame has started loading");
+                }
+            }
+            
+            @Override
+            public void onFinishLoadingFrame(FinishLoadingEvent event) {
+                if (event.isMainFrame()) {
+                    System.out.println("Main frame has finished loading");
+                }
+            }
+        });
+        
+                
         Composite composite = new Composite(shell, SWT.EMBEDDED | SWT.NO_BACKGROUND);
-        composite.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_CENTER));
-        GridLayout layout = new GridLayout();
-        layout.numColumns = 3;
-        composite.setLayout(layout);
+
         Frame frame = SWT_AWT.new_Frame(composite);
         BrowserView bv = new BrowserView(browser);
         JApplet applet = new JApplet();
+        
         applet.add(bv);
         frame.add(applet);
     
-        shell.setSize(800, 600);
+        shell.setSize(1200, 800);
         shell.open();
         return shell;
     }
@@ -76,7 +112,8 @@ public class SwtDemo {
         subItem.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                browser.loadURL("http://www.google.com");
+                browser.loadURL("http://www.yahoo.com");
+                swtBrowser.setUrl("http://www.yahoo.com");
             }
         });
 
